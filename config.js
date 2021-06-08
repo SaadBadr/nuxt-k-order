90a01b0e6317526c3c0e6d0bd1d26879cba10ded
SaadBadr <saadbadr23@gmail.com>
2021-06-08 19:52:52 +0200

    adding production

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-export const baseURL = 'http://127.0.0.1:8000/api/v1'
+console.log(process.env.NODE_ENV)
+export const baseURL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://127.0.0.1:8000/api/v1'
+    : 'http://www.korder.com/api/v1'
